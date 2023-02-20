358b89e5814f369db7894fc4cea7dc4402ea171f
zhaohengsheng <1787600886@qq.com>
2023-02-20 17:51:33 +0800

    call

--- pages/20230215/callApplyBind.vue
+++ pages/20230215/callApplyBind.vue
@@ -7,22 +7,23 @@
  * call
  *  接收的第一个参数是
  */
-//call接受1个指定上下文对象和多个参数
-Function.prototype.myCall = function (context, ...args) {
-    if (!(context instanceof Object)) context = new Object(context)
-
-    console.log(context)
-    //创建一个唯一的key，防止覆盖上下文对象里的数据
-    let soleKey = Symbol()
-    //把要调用的函数存到指定上下文对象中（这里的this是要调用的函数）
-    context[soleKey] = this
-    //在指定上下文对象中调用，this指向了context
-    let res = context[soleKey](...args)
-    //调用后删除存入的函数
-    delete context[soleKey]
-    //返回调用函数的返回值
-    return res
-}
+// //call接受1个指定对象和多个参数
+// Function.prototype.myCall = function (context, ...args) {
+//   	//传入的是null或undefined 指向全局
+//   	if(context===null||context===undefined) context = window
+//   	//如果传入的是原始类型值就包装一下
+//     if (!(context instanceof Object)) context = new Object(context)
+//     //创建一个唯一的key，防止覆盖对象里的数据
+//     let soleKey = Symbol()
+//     //把要调用的函数存到指定对象中
+//     context[soleKey] = this
+//     //在指定上下文对象中调用，this指向了context
+//     let res = context[soleKey](...args)
+//     //调用后删除存入的函数
+//     delete context[soleKey]
+//     //返回调用函数的返回值
+//     return res
+// }
 let fun = function (a, b, c) {
     console.log("this", this)
     console.log("args", a, b, c)
@@ -35,26 +36,24 @@ let obj = {
     name: "铛铛",
     age: 18,
 }
-fun.myCall(obj, 1, 2, 3)
-// fun()
-// fun.call("xxx")
+let obj2 = {
+    name: "啪啪",
+    age: 19,
+}
+// //myCall的上下文对象是fun
+// fun.myCall(obj, 1, 2, 3)//this {name: '铛铛', age: 18, Symbol(): ƒ}   args 1 2 3
 
 // bind
-// Function.prototype.myBind = function (oThis, ...args) {
-//     console.log(oThis)
-//     console.log(this.prototype)
-//     return
-//     let fTobind = this,
-//         FNOP = function () {},
-//         fBound = function () {
-//             return fTobind.call(this instanceof FNOP && oThis ? this : oThis, ...args)
-//         }
-//     FNOP.prototype = this.prototype
-//     fBound.prototype = new FNOP()
-//     return fBound
-// }
-// fun.myBind(obj)
-// fun()
+Function.prototype.myBind = function (context, ...args1) {
+    return (...args2) => {
+        console.log(this)
+        return this.call(context, ...args1, ...args2)
+    }
+}
+let fn = fun.myBind(obj)
+fn()
+let fn2 = fun.myBind(obj2)
+fn2()
 </script>
 
 <style lang="scss" scoped></style>
