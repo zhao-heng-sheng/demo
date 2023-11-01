dcfe19844f65c2b2cea81c87f4bcf91a637a56c1
zhaohengsheng <1787600886@qq.com>
2023-11-01 17:55:36 +0800

    ==>2023-11-01 17:55:31 | auto push

--- node/nest/gateway/src/main.ts
+++ node/nest/gateway/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {FastifyAdapter,NestFastifyApplication} from '@nestjs/platform-fastify'
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter());
+  // 接口版本化管理
+  app.enableVersioning({
+    type:VersioningType.URI
+  })
   await app.listen(3000);
 }
 bootstrap();
--- node/nest/gateway/src/user/user.controller.ts
+++ node/nest/gateway/src/user/user.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Version } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    getHello(){
-        return 'hello user'
-    }
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @Version('1')
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
 }
