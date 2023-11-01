07bb0bc55312ca71a7317adcaac7d36089c64b11
zhaohengsheng <1787600886@qq.com>
2023-11-01 17:30:55 +0800

    ==>2023-11-01 17:30:51 | auto push

--- node/nest/gateway/src/app.module.ts
+++ node/nest/gateway/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
