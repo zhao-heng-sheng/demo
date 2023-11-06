b4747b057ac34c52f945c7e831a9acb27a2880ac
zhaohengsheng <1787600886@qq.com>
2023-11-06 14:01:42 +0800

    ==>2023-11-06 14:01:38 | auto push

---
+++ node/nest/gateway/src/user/user.mongo.entity.ts
@@ -0,0 +1,8 @@
+import { Entity, Column, UpdateDateColumn, ObjectIdColumn } from 'typeorm';
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  id?: number;
+  @Column({ default: null })
+  name: string;
+}
