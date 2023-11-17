d5adc1a992e9dffb51bae0d134d6e23e050688f8
zhaohengsheng <1787600886@qq.com>
2023-11-17 16:07:29 +0800

    ==>2023-11-17 16:07:24 | auto push

--- JavaScript/proxy/3.js
+++ JavaScript/proxy/3.js
@@ -1,11 +1,7 @@
 const target = {foo:'bar'}
 const handler = {
     get(trapTarget,key,receiver){
-        console.log(target===trapTarget,key,receiver===proxy);
-        // return trapTarget[key]
         return Reflect.get(...arguments)
     }
 }
 const proxy = new Proxy(target,handler)
-console.log(target.foo);
-console.log(proxy.foo);
