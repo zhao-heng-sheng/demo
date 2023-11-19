a32c3489eeff08b9278890adfc5bed6616f9180e
zhaohengsheng <1787600886@qq.com>
2023-11-19 21:35:02 +0800

    ==>2023-11-19 21:34:58auto push

--- node/nest/zhaohs/src/app.service.ts
+++ node/nest/zhaohs/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    let a = 2;
     return 'Hello World!';
   }
 }
--- node/nest/zhaohs/src/main.ts
+++ node/nest/zhaohs/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
+import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('zhaohs swagger')
+    .setDescription('swagger文档')
+    .setVersion('1.0')
+    .addTag('xxx')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
+  log('http://localhost:3000');
 }
 bootstrap();
