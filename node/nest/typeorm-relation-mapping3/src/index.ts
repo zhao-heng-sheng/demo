cdbbf1506e50daef84cc9f199e037b0f96a320ba
zhaohengsheng <1787600886@qq.com>
2023-11-23 21:19:04 +0800

    ==>2023-11-23 21:18:59auto push

--- node/nest/typeorm-relation-mapping3/src/index.ts
+++ node/nest/typeorm-relation-mapping3/src/index.ts
@@ -5,14 +5,27 @@ import { Tag } from './entity/Tag';
 
 AppDataSource.initialize().then(async () => {
 
-    const a1 = new Article();
-    a1.title = '文章1';
-    a1.content = '文章1内容';
-    const a2 = new Article();
-    a2.title = '文章2';
-    a2.content = '文章2内容';
-    const t1 = new Tag();
-    t1.name ="标签1";
-    const t2 = new Tag();
+    // const a1 = new Article();
+    // a1.title = '文章1';
+    // a1.content = '文章1内容';
+    // const a2 = new Article();
+    // a2.title = '文章2';
+    // a2.content = '文章2内容';
+    // const t1 = new Tag();
+    // t1.name ="标签1";
+    // const t2 = new Tag();
+    // t2.name = '标签2'
+    // const t3 = new Tag();
+    // t3.name = '标签3'
+    // a1.tags = [t1,t2];
+    // a2.tags = [t2,t3];
+    // const entityMananger = AppDataSource.manager;
+    // await entityMananger.save([t1,t2,t3,a1,a2]);
+
+
+    const article = await AppDataSource.manager.findOne(Article,{
+        relations:['tags']
+    })
+    console.log(article)
 
 }).catch(error => console.log(error))
