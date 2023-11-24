07f3d94224040dbd05c7c6c0576efa33d836843b
zhaohengsheng <1787600886@qq.com>
2023-11-24 21:14:12 +0800

    ==>2023-11-24 21:14:07auto push

--- JavaScript/Map/5.js
+++ JavaScript/Map/5.js
@@ -3,3 +3,13 @@ const m = new Map([
     ["key2", "val2"],
 ]);
 console.log(m.entries===m[Symbol.iterator]);
+// m.forEach(item=>{
+//     console.log(item);
+// })
+// m.forEach((value,key,map)=>{
+//     console.log(value,key,map);
+// })
+let entries = m.entries();
+console.log(entries.next());
+console.log(entries.next());
+console.log(entries.next());
