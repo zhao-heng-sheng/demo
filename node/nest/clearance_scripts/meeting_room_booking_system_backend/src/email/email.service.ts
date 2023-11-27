cf340f4d891ce12656ebc80d8c6a6ae22f52642c
zhaohengsheng <1787600886@qq.com>
2023-11-27 20:52:10 +0800

    ==>2023-11-27 20:52:05auto push

--- node/nest/clearance_scripts/meeting_room_booking_system_backend/src/email/email.service.ts
+++ node/nest/clearance_scripts/meeting_room_booking_system_backend/src/email/email.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { createTransport, Transporter} from 'nodemailer';
 @Injectable()
-export class EmailService {}
+export class EmailService {
+    transporter:Transporter
+    constructor(){
+        this.transporter = createTransport({
+            host: 'smtp.qq.com',
+            port:587,
+            secure:false,
+            auth:{
+                user:'',
+                pass:''
+            },
+        })
+    }
+}
