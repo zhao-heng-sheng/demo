f7685b2d1e424a6d5ab1281dfb87544d73f908a7
zhaohengsheng <1787600886@qq.com>
2023-12-01 22:13:56 +0800

    ==>2023-12-01 22:13:51auto push

---
+++ JavaScript/OOP/forin.js
@@ -0,0 +1,20 @@
+let Person = function(obj){
+    // ({name:this.name,age:this.age} = obj)
+}
+Person.prototype.name = 'zhangsan';
+Person.prototype.age = 20;
+Person.prototype.say = function(){
+    console.log('hello'+this.name);
+}
+Object.defineProperty(Person.prototype,'name',{
+    enumerable:false
+})
+let p = new Person();
+let symbol = Symbol('symbol');
+p.job = 'student';
+p[symbol] = 'symbol';
+for(let key in p){
+    console.log(key);
+}
+console.log(Object.keys(p));
+console.log(p);
