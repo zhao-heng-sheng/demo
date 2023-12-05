51e54cb18be995d58dfd92505d582abe87f45cf1
zhaohengsheng <1787600886@qq.com>
2023-12-05 17:57:52 +0800

    ==>2023-12-05 17:57:48 | auto push

--- node/nest/clearance_scripts/meeting_room_booking_system_backend/src/app.module.ts
+++ node/nest/clearance_scripts/meeting_room_booking_system_backend/src/app.module.ts
@@ -9,7 +9,7 @@ import { Permission } from './user/entities/permission.entity';
 import { RedisModule } from './redis/redis.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
@@ -33,11 +33,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       },
       inject: [ConfigService],
     }),
-    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: 'src/.env',
     }),
+    JwtModule.registerAsync({
+      global: true,
+      useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get('jwt_secret'),
+          signOptions: {
+            expiresIn: '30m',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
     RedisModule,
     EmailModule,
   ],
