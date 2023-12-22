d5eaee9328998eef2a0be598f8a9e37e7a8aa1dd
zhaohs <1787600886@qq.com>
2023-12-22 12:17:32 +0800

    ==>2023-12-22 12:17:27 | auto push

--- HTML/all-history-extend/content.ts
+++ HTML/all-history-extend/content.ts
@@ -2,29 +2,35 @@ import axios from "axios"
 
 import { getBsInfo, getOS } from "./utils"
 
+console.log(event)
 ;(async () => {
-  console.log(navigator, "123")
-  console.log(getBsInfo(), "xxx")
-  let browserType = getBsInfo().bs_name
   let { href: url, hostname: domain, port } = window.location
+  let browserType = getBsInfo().bs_name
   let title = document.title
   let os = getOS()
   let latitude, longitude
-
-  axios
-    .post("http://localhost:3000/history", {
-      url,
-      domain,
-      port,
-      title,
-      browserType,
-      os,
-      latitude,
-      longitude
-    })
-    .then((res) => {
-      console.log(res)
-    })
+  let addHistory = ()=>{
+    axios
+      .post("http://localhost:3000/history", {
+        url,
+        domain,
+        port,
+        title,
+        browserType,
+        os,
+        latitude,
+        longitude
+      })
+      .then((res) => {
+        console.log(res)
+      })
+  }
+  addHistory();
+  window.addEventListener('popstate',(e)=>{
+    console.log(e);
+    title = document.title
+    addHistory();
+  })
 })()
 
 export {}
