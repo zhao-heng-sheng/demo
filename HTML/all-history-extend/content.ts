7ee1be9af12ac91f0fb8fce9de660680a50386f9
zhaohs <1787600886@qq.com>
2023-12-22 14:17:40 +0800

    ==>2023-12-22 14:17:35 | auto push

--- HTML/all-history-extend/content.ts
+++ HTML/all-history-extend/content.ts
@@ -2,14 +2,30 @@ import axios from "axios"
 
 import { getBsInfo, getOS } from "./utils"
 
-console.log(event)
+const _historyWrap = function (type) {
+  const orig = history[type]
+  const e = new Event(type)
+  return function () {
+    const rv = orig.apply(this, arguments)
+    e.arguments = arguments
+    window.dispatchEvent(e)
+    return rv
+  }
+}
+history.pushState = _historyWrap("pushState")
+history.replaceState = _historyWrap("replaceState")
+
 ;(async () => {
   let { href: url, hostname: domain, port } = window.location
   let browserType = getBsInfo().bs_name
   let title = document.title
   let os = getOS()
   let latitude, longitude
-  let addHistory = ()=>{
+  let regainBrowserData = () => {
+    ;({ href: url, hostname: domain, port } = window.location)
+    title = document.title
+  }
+  let addHistory = () => {
     axios
       .post("http://localhost:3000/history", {
         url,
@@ -25,11 +41,24 @@ console.log(event)
         console.log(res)
       })
   }
-  addHistory();
-  window.addEventListener('popstate',(e)=>{
-    console.log(e);
-    title = document.title
-    addHistory();
+  addHistory()
+  window.addEventListener("hashchange", (e) => {
+    console.log('hashchange');
+    
+    regainBrowserData()
+    addHistory()
+  })
+  window.addEventListener("pushState", function (e) {
+    console.log('pushState');
+    
+    regainBrowserData()
+    addHistory()
+  })
+  window.addEventListener("replaceState", function (e) {
+    console.log('replaceState');
+    
+    regainBrowserData()
+    addHistory()
   })
 })()
 
