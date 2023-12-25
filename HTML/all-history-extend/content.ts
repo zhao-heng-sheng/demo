4f638c1f9d356bee65099cd8669407419cc9b921
zhaohs <1787600886@qq.com>
2023-12-25 15:12:22 +0800

    ==>2023-12-25 15:12:00 | auto push

--- HTML/all-history-extend/content.ts
+++ HTML/all-history-extend/content.ts
@@ -15,7 +15,7 @@ import { getBsInfo, getOS } from "./utils"
   let addHistory = () => {
     let notRecordDomain = ["localhost", "127.0.0.1"]
     if (notRecordDomain.includes(domain)) return
-    axios.post("http://localhost:3000/history", {
+    axios.post(`${process.env.REQ_DOMAIN}/history`, {
       url,
       domain,
       port,
@@ -25,9 +25,6 @@ import { getBsInfo, getOS } from "./utils"
       latitude,
       longitude
     })
-    // .then((res) => {
-    //   console.log(res)
-    // })
   }
   addHistory()
   window.addEventListener("hashchange", (e) => {
