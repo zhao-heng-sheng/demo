b24d5877cd92b38cc31e33646de2a2159d0982e4
zhaohs <1787600886@qq.com>
2024-02-27 17:57:55 +0800

    ==>2024-02-27 17:57:50 | auto push

---
+++ JavaScript/other/1.js
@@ -0,0 +1,12 @@
+function add(arr){
+  return arr.map(item=>item+1)
+}
+function mul(arr){
+  return arr.map(item=>item*3)
+}
+function div(arr){
+  return arr.map(item=>item/2)
+}
+console.log(add([1,2,3]))
+console.log(mul([1,2,3]))
+console.log(div([2,4,6]))
