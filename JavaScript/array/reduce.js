7589eb1506c35bdafff697a509a804c275796920
zhaohs <1787600886@qq.com>
2024-02-28 16:18:27 +0800

    ==>2024-02-28 16:18:22 | auto push

--- JavaScript/array/reduce.js
+++ JavaScript/array/reduce.js
@@ -3,4 +3,22 @@ const initialValue = 0;
 const add = (previousValue, currentValue) => previousValue + currentValue;
 const sumArr = arr.reduce(add, initialValue);
 console.log(sumArr);
-console.log(arr);
+
+const map = (arr,callback)=>{
+  let result = [];
+  for(let i=0;i<arr.length;i++){
+    result.push(callback(arr[i]));
+  }
+  return result;
+}
+let sumArr2 = map(arr,item=>item+1)
+console.log(sumArr2);
+let initialValue2 = []
+let sumArr3 = arr.reduce((previousValue,currentValue)=>{
+  // console.log(previousValue,currentValue);
+  let result = previousValue.slice()
+  result.push(currentValue+1)
+  return result;
+},initialValue2)
+console.log(sumArr3);
+console.log(initialValue2);
