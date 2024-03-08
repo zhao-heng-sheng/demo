bca670afef048116dfdac254b46ae80a5bede955
zhaohengsheng <1787600886@qq.com>
2024-03-08 21:15:44 +0800

    ==>2024-03-08 21:15:39auto push

--- other/class_timetable/script.js
+++ other/class_timetable/script.js
@@ -2,10 +2,17 @@
 const fs = require("fs");
 // 引入node-xlsx
 const xlsx = require("node-xlsx");
+const dayjs = require("dayjs");
 // 读取excel文件
 let rateXlsx = xlsx.parse(__dirname + "/1.xlsx")[0].data;
-console.log(rateXlsx);
-debugger;
+
+let dateDispose = function (text) {
+    console.log(text);
+    let arr = text.split("\n");
+    // console.log(arr);
+    if (arr.length == 1) return `${dayjs().year()}/${arr[0]}`;
+    if (arr.length == 2) return `${dayjs().year()}/${arr[1]}`;
+};
 /**
  *
  * @param {Array} rateArr  表格数据
@@ -20,14 +27,14 @@ let pushDate = function (rateArr, min, max) {
         //行循环
         for (let j = 12; j < rateArr[i].length; j++) {
             //列循环
-            let insuredAge = rateArr[i][0],
-                gender = getGenderCode(rateArr, j),
-                chargeCode = getChargeCode(rateArr[3][j]),
-                ageClaim = getAgeClaim(rateArr),
-                coefficient = getCoefficient(rateArr);
-            if ((rateArr[i][j] && rateArr[i][j] == "面授" && rateArr[i][j] == "Z") || rateArr[i][j] == "z") {
-                data.push([insuredAge, gender, chargeCode, ageClaim, coefficient, ":", rateArr[i][j]]);
-                hash[`insuredAge_${insuredAge}|gender_${gender}|chargeCode_${chargeCode}|annuityReceivingAge_${ageClaim}|coefficient_${coefficient}`] = rateArr[i][j];
+            let subject = rateArr[i][2],
+                startTime = rateArr[i][10].split("-")[0],
+                endTime = rateArr[i][10]?.split("-")[1]?.split("(")[0];
+            if (rateArr[i][j]) {
+                let startDate = dateDispose(rateArr[2][j]),
+                    endDate = dateDispose(rateArr[2][j]),
+                    location = rateArr[i][j].indexOf("面授") + 1 ? "面授" : "线上";
+                data.push([subject, startDate, endDate, startTime, endTime, location]);
             }
         }
     }
