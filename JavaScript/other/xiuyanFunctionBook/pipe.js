ec306836aed5493517fe4882dffdacd40d42f9ed
zhaohs <1787600886@qq.com>
2024-03-13 11:24:45 +0800

    2024-03-13 11:24:40 | auto push

--- JavaScript/other/xiuyanFunctionBook/pipe.js
+++ JavaScript/other/xiuyanFunctionBook/pipe.js
@@ -1,19 +1,17 @@
-function pipe(...args){
-  let callback = (num,fun)=>{
-    return fun(num)
-  }
-  return (num)=>{
-    return args.reduce(callback,num)
-  }
+function pipe(...fns) {
+    let callback = (val, fn) => fn(val);
+    return function (arg) {
+        return fns.reduce(callback, arg);
+    };
 }
-function add(num){
-  return num +3
+function add(num) {
+    return num + 3;
 }
-function multiply(num){
-  return num * 3
+function multiply(num) {
+    return num * 3;
 }
-function divide(num){
-  return num / 2
+function divide(num) {
+    return num / 2;
 }
-let x = pipe(add, multiply, divide)
-console.log(x(3))
+let x = pipe(add, multiply, divide);
+console.log(x(3));
