83e147cb7eca39339ab17f3ed758c68019b8b52f
zhaohs <1787600886@qq.com>
2024-03-13 15:24:38 +0800

    2024-03-13 15:24:33 | auto push

--- JavaScript/other/xiuyanFunctionBook/19.js
+++ JavaScript/other/xiuyanFunctionBook/19.js
@@ -1,4 +1,12 @@
 const isExisted = (id) => id % 3 === 0;
+const isEmpty = (val) => val === null || val === undefined;
+const Maybe = (val) => {
+    return {
+        map: (fn) => (isEmpty(val) ? Maybe(null) : Maybe(fn(val))),
+        valueOf: () => val,
+        inspect: () => `Maybe(${val})`,
+    };
+};
 
 const getUser = (id) => {
     if (isExisted(id)) {
@@ -17,4 +25,5 @@ const getUserSafely = (id) => {
     }
 };
 
-const res = getUserSafely(1110021)
+const res = getUserSafely(1110021);
+console.log(res.valueOf());
