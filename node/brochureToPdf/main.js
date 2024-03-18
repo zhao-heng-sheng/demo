f49dbf1516b757be5b79fb2fcbc2b118636612e1
zhaohs <1787600886@qq.com>
2024-03-18 10:06:31 +0800

    2024-03-18 10:06:27 | auto push

--- node/brochureToPdf/main.js
+++ node/brochureToPdf/main.js
@@ -3,8 +3,8 @@ const { bookLinks } = require("./config");
 const runCrawling = require("./runCrawling");
 (async () => {
     const browser = await puppeteer.launch({
-        // headless: false,
-        headless:true,
+        headless: false,
+        // headless:true,
         defaultViewport: { width: 1920, height: 199999 },
     });
     for(bookLink of bookLinks){
