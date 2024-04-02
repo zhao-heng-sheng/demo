92669544fb15ca52e93339bcb2ed0ae0d4a4ef10
zhaohs <1787600886@qq.com>
2024-04-02 10:18:25 +0800

    2024-04-02 10:18:21 | auto push

--- other/go_over/2024年3月29日/1.js
+++ other/go_over/2024年3月29日/1.js
@@ -1,2 +1,20 @@
-let val1 = a[1][2][3] + 4; //10
-let val2 = a[10][20][30] + 40;
+let jia = (val) => {
+    return val + 1;
+};
+let jian = (val) => {
+    return val - 2;
+};
+
+let cheng = (val) => {
+    return val * 3;
+};
+
+let pipe = (...fns) => {
+    let callback = (val, fn) => fn(val);
+    return (val) => {
+        return fns.reduce(callback, val);
+    };
+};
+
+let res = pipe(jia, jian, cheng);
+console.log(res(3));
