f07ced4a75100367e13c24ff77d2f2096193acd5
zhaohs <1787600886@qq.com>
2024-04-08 16:29:53 +0800

    2024-04-08 16:29:48 | auto push

--- other/go_over/2024年4月7日/3.js
+++ other/go_over/2024年4月7日/3.js
@@ -4,6 +4,14 @@ const weakmap = new WeakMap();
 (()=>{
   const foo = {foo:1}
   const bar = {bar:2}
+  function a(){
+    let a = '123'
+    return ()=>{
+      weakmap.set(a,3)
+      return a;
+    }
+  }
+  let b = a()
   map.set(foo,1)
   weakmap.set(bar,2)
   console.log(map,weakmap);
