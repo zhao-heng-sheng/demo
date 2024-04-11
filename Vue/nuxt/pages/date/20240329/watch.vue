20c7a688fb3a858b8bc14ee6b45a91f8bdcfb3f3
zhaohs <1787600886@qq.com>
2024-04-11 09:16:24 +0800

    2024-04-11 09:16:19 | auto push

--- Vue/nuxt/pages/date/20240329/watch.vue
+++ Vue/nuxt/pages/date/20240329/watch.vue
@@ -1,9 +1,43 @@
 <template>
-    <div></div>
+    <p>年龄：{{ age }}</p>
+    <p>车：{{ car.brand }}</p>
+    <p>房子地址：{{ house.address }}</p>
+    <button @click="age++">年龄调整</button><br>
+    <button @click="car.brand =car.brand+'~'">车调整</button><br>
+    <button @click="house.address =house.address+'~'">房调整</button><br>
 </template>
 
 <script setup>
-import { watchEffect } from "vue";
+import { watch } from "vue";
+let age = ref(18);
+let name = ref("张三");
+let car = reactive({
+    brand: "奔驰",
+    price: 1000000,
+});
+let house = ref({
+    address: "北京",
+    price: 10000000,
+})
+watch(
+    age,
+    (val) => {
+        console.log(val);
+    },
+    {}
+);
+watch(car, (val) => {
+    console.log(val);
+});
+// watch(house, (val) => {
+//     console.log(val);
+// },{deep:true});
+watch(
+    () => house.value.address,
+    (val) => {
+        console.log(val);
+    }
+);
 </script>
 
 <style lang="scss" scoped></style>
