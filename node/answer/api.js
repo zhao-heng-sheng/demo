b3aea8d87c40aec0652749d6d1b5d221ee0f7b3a
zhaohengsheng <1787600886@qq.com>
2024-04-13 12:35:32 +0800

    ==>2024-04-13 12:35:27auto push

--- node/answer/api.js
+++ node/answer/api.js
@@ -1,35 +1,35 @@
-import axios from "./axios.js"
-import path from 'path'
-import { JsonDB, Config } from 'node-json-db';
+import axios from "./axios.js";
+import path from "path";
+import { JsonDB, Config } from "node-json-db";
 const __dirname = path.dirname(import.meta.url);
-let db = new JsonDB(new Config('bank', true, true, '/'));
+let db = new JsonDB(new Config("bank", true, true, "/"));
 /**
  * 作业列表
   @params {Object} params
   @params {String} params.courseId 课程id
   @params {String} params.pageIndex 页码
 */
-export let loadTopicListData = (params)=>{
-  return axios.post('https://www.learnin.com.cn/app/user/student/course/space/topic/appStudentCourseTopic/loadTopicListData',params)
-}
+export let loadTopicListData = (params) => {
+    return axios.post("https://www.learnin.com.cn/app/user/student/course/space/topic/appStudentCourseTopic/loadTopicListData", params);
+};
 /**
  * 习题列表
   @params {Object} params
   @params {String} params.courseId 课程id
   @params {String} params.topicId 作业id
 */
-export let loadTopicData = (params)=>{
-  return axios.post('https://www.learnin.com.cn/app/user/student/course/space/topic/appStudentCourseTopic/loadTopicData',params)
-}
+export let loadTopicData = (params) => {
+    return axios.post("https://www.learnin.com.cn/app/user/student/course/space/topic/appStudentCourseTopic/loadTopicData", params);
+};
 
 //查询db答案
-export let getAnswer = (params)=>{
-  // console.log('getAnswer',params);
-  return db.getData(`/`)
-}
+export let getAnswer = async (id) => {
+    let res = await db.getData(`/answer`);
+    return res.find((item) => item.id === id);
+};
 
 // 保存答案到db
-export let saveAnswer = (params)=>{
-  console.log('saveAnswer',params);
-  return db.push(`/answer[]`,params)
-}
+export let saveAnswer = (params) => {
+    console.log("saveAnswer", params);
+    return db.push(`/answer[]`, params);
+};
--- node/answer/main.js
+++ node/answer/main.js
@@ -1,9 +1,29 @@
+import { disciplines } from "./data.js";
+import { loadTopicListData, loadTopicData, saveAnswer, getAnswer } from "./api.js";
 
-import {disciplines} from './data.js'
-import {loadTopicListData,saveAnswer,getAnswer} from './api.js'
+let getTopicList = async (courseId) => {
+    let limit = 10;
+    let countPage = 1;
+    let totalCount = 0;
+    let topicList = [];
+    let firstRes = await loadTopicListData({ courseId, pageIndex: 1 });
+    topicList.push(...firstRes.topics);
+    totalCount = firstRes.totalCount;
+    countPage = Math.ceil(totalCount / limit);
+    let res = await Promise.all([...Array(countPage - 1).keys()].map((i) => loadTopicListData({ courseId, pageIndex: i + 2 })));
+    res.forEach((item) => {
+        topicList.push(...item.topics);
+    });
+    return topicList;
+};
+let getTopicData = async (courseId, topicId) => {
+    let res = await loadTopicData({ courseId, topicId });
+    return res;
+};
 
-saveAnswer({a:3,c:2})
-// (async()=>{
-//   let res = await loadTopicListData({courseId:disciplines[0].id,pageIndex:1})
-//   console.log(res)
-// })()
+(async () => {
+    let topicList = await getTopicList(disciplines[0].id);
+    // console.log(topicList, 'topicList');
+    let topicData = await loadTopicData({ courseId: disciplines[0].id, topicId: topicList[0].id });
+    console.log(topicData, "topicData");
+})();
