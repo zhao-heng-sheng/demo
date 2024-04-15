b6e0a1d27a87d4e213477ebaea314b580a8b2229
zhaohs <1787600886@qq.com>
2024-04-15 09:09:42 +0800

    2024-04-15 09:09:35 | auto push

--- other/go_over/2024年4月12日/2.js
+++ other/go_over/2024年4月12日/2.js
@@ -1,6 +1,10 @@
 class Storage{
-    setItem(key,value){}
-    getItem(key){}
+    setItem(key,value){
+        return localStorage.setItem(key,value);
+    }
+    getItem(key){
+        return localStorage.getItem(key);
+    }
     static getInstance(){
         if(!Storage.instance){
             Storage.instance = new Storage();
