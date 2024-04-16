ad3d12a1778b64da8c89de57f66549cd8ecea689
zhaohs <1787600886@qq.com>
2024-04-16 11:17:02 +0800

    2024-04-16 11:16:58 | auto push

--- other/go_over/2024年4月16日/2.js
+++ other/go_over/2024年4月16日/2.js
@@ -1,22 +1,31 @@
-let deepCopy = (obj)=>{
-
-}
-let symbol = Symbol()
+let deepCopy = (obj) => {
+    let newObj = obj instanceof Array ? [] : {};
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] === "object" && obj[key] !== null) {
+                newObj[key] = deepCopy(obj[key]);
+            }else{
+              newObj[key] = obj[key];
+            }
+        }
+    }
+    return newObj;
+};
+let symbol = Symbol();
 let obj = {
-  a:1,
-  b:{
-    c:3,
-    d:4
-  },
-  e:[1345,3345,4345,565756],
-  f:symbol,
-  g:undefined,
-  h:null,
-  i:NaN,
-  j:Infinity,
-  k:()=>{},
-  l:/\d/,
-  m:new Date(),
-  n:Math,
-  o:window,
-}
+    a: 1,
+    b: {
+        c: 3,
+        d: 4,
+    },
+    e: [1345, 3345, 4345, 565756],
+    f: symbol,
+    g: undefined,
+    h: null,
+    i: NaN,
+    k: () => {},
+    l: /\d/,
+    m: new Date(),
+};
+let obj2 = deepCopy(obj);
+console.log(obj2);
