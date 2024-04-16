17ad4cd2bc2137f47c19988ff1cef023244d6d5f
zhaohs <1787600886@qq.com>
2024-04-16 10:17:05 +0800

    2024-04-16 10:17:00 | auto push

---
+++ other/go_over/2024年4月16日/2.js
@@ -0,0 +1,22 @@
+let deepCopy = (obj)=>{
+
+}
+let symbol = Symbol()
+let obj = {
+  a:1,
+  b:{
+    c:3,
+    d:4
+  },
+  e:[1345,3345,4345,565756],
+  f:symbol,
+  g:undefined,
+  h:null,
+  i:NaN,
+  j:Infinity,
+  k:()=>{},
+  l:/\d/,
+  m:new Date(),
+  n:Math,
+  o:window,
+}
