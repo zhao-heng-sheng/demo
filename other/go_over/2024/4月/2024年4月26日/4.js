0f52413d569859f4207d5d86bbf0f5fa232e92f3
zhaohs <console.log@163.com>
2024-04-26 15:14:33 +0800

    2024-04-26 15:14:28 | auto push

--- other/go_over/2024/4月/2024年4月26日/4.js
+++ other/go_over/2024/4月/2024年4月26日/4.js
@@ -9,11 +9,11 @@
 输入: 1->1->1->2->3
 输出: 2->3
 */
-class ListNode{
-  constructor(val){
-    this.val = val
-    this.next = null
-  }
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 let node = new ListNode(1);
 node.next = new ListNode(1);
@@ -22,16 +22,21 @@ node.next.next.next = new ListNode(2);
 node.next.next.next.next = new ListNode(3);
 node.next.next.next.next.next = new ListNode(3);
 
-let fn = (node)=>{
-	let head = node;
-	while(head&&head.next){
-		if(head.val===head.next.val){
-			head = head.next.next
-		}else{
-			head = head.next
-		}
-	}
-  console.log(head);
-	return node
-}
-console.log(fn(node))
+let fn = (head) => {
+    // 设定 cur 指针，初始位置为链表第一个结点
+    let cur = head;
+    // 遍历链表
+    while (cur != null && cur.next != null) {
+        // 若当前结点和它后面一个结点值相等（重复）
+        if (cur.val === cur.next.val) {
+            console.log("xxx");
+            // 删除靠后的那个结点（去重）
+            cur.next = cur.next.next;
+        } else {
+            // 若不重复，继续遍历
+            cur = cur.next;
+        }
+    }
+    return head;
+};
+console.log(fn(node));
