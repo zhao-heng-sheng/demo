8fb1bc19e7fb583ab3759909668bd75cfd8c33a2
zhaohs <console.log@163.com>
2024-04-26 16:14:30 +0800

    2024-04-26 16:14:25 | auto push

--- other/go_over/2024/4月/2024年4月26日/4.js
+++ other/go_over/2024/4月/2024年4月26日/4.js
@@ -23,20 +23,21 @@ node.next.next.next.next = new ListNode(3);
 node.next.next.next.next.next = new ListNode(3);
 
 let fn = (head) => {
+    let node = new ListNode(0)
+    node.next = head
     // 设定 cur 指针，初始位置为链表第一个结点
-    let cur = head;
+    let cur = node;
     // 遍历链表
     while (cur != null && cur.next != null) {
         // 若当前结点和它后面一个结点值相等（重复）
         if (cur.val === cur.next.val) {
-            console.log("xxx");
             // 删除靠后的那个结点（去重）
-            cur.next = cur.next.next;
+            cur.next = cur.next.next?.next;
         } else {
             // 若不重复，继续遍历
             cur = cur.next;
         }
     }
-    return head;
+    return node;
 };
 console.log(fn(node));
