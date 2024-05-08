ad3635dddaac208ec4e488458ac9abc891de4df1
zhaohs <console.log@163.com>
2024-05-08 10:13:18 +0800

    2024-05-08 10:13:12 | auto push

--- node/replaceRed/key.js
+++ node/replaceRed/key.js
@@ -1,12 +1,17 @@
 import fs from 'fs'
 import * as cheerio from 'cheerio';
-const html = fs.readFileSync('./source.html', 'utf8')
-const color = '#FF0000'
+const html = fs.readFileSync('./source.html', 'utf-8')
+// console.log(html);
+const color = '#ff3300'
 const $ = cheerio.load(html)
-const elements = $(`[style*="color:${color}"]`)
+const elements = $(`[style*="${color}"]`)
+// const elements = $(`#u372`)
 let keys = []
 elements.each((i, element) => {
-    keys.push($(element).text())
+    if($(element).text()){
+        keys.push($(element).text())
+    }
 })
+console.log(keys);
 // 简单点，直接去重,匹配所有key并替换
 export default [...new Set(keys)]
