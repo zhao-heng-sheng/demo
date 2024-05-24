337d33bd12d67f03dff8e9564b8f36c07ca74c22
zhaohs <console.log@163.com>
2024-05-24 09:14:12 +0800

    2024-05-24 09:14:06 | auto push

--- React/xiaoce/controlled-and-uncontrolled/src/App.tsx
+++ React/xiaoce/controlled-and-uncontrolled/src/App.tsx
@@ -1,10 +1,35 @@
-import {ChangeEvent} from "react"
+import { ChangeEvent, useState } from "react";
 
-export default function App(){
-  function onChange(event: ChangeEvent<HTMLInputElement>){
-    console.log(event.target.value)
-  }
-  return (
-    <input defaultValue={'guang'} onChange={onChange} />
-  )
+interface CalendarProps {
+    defaultValue?: Date;
+    onChange: (date: Date) => void;
 }
+
+function Calendar(props: CalendarProps) {
+    const { defaultValue = new Date(), onChange } = props;
+    const [value,setValue] = useState(defaultValue)
+    let changeValue = (date:Date)=>{
+      setValue(date);
+      onChange?.(date)
+    }
+    return (
+      <div>
+        {value.toLocaleDateString()}
+        <div onClick={()=>changeValue(new Date('2024-3-1'))}>2024-3-1</div>
+        <div onClick={()=>changeValue(new Date('2024-3-2'))}>2024-3-2</div>
+        <div onClick={()=>changeValue(new Date('2024-3-3'))}>2024-3-3</div>
+      </div>
+    )
+}
+
+function App() {
+    return (
+        <Calendar
+            defaultValue={new Date("2024-1-4")}
+            onChange={(date) => {
+                console.log(date.toLocaleDateString());
+            }}
+        />
+    );
+}
+export default App;
