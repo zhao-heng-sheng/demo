9416c5355510cbb0e3f9fd95ce5ce7cfacbe8c26
zhaohs <console.log@163.com>
2024-05-29 10:21:49 +0800

    2024-05-29 10:21:44 | auto push

---
+++ Vue/nuxt/pages/date/20240529/X.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot name="xxx">
+
+    </slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue/nuxt/pages/date/20240529/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <X>
+      <template #xxx>
+        <div ref="c">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </div>
+      </template>
+    </X>
+  </div>
+</template>
+
+<script setup>
+import X from './x.vue'
+import { ref } from 'vue'
+const c = ref(null)
+onMounted(() => {
+  console.log(c)
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
