9e882c1c8dc4f6ff20e420bcb1a437df4851e070
zhaohs <console.log@163.com>
2024-06-04 11:25:31 +0800

    2024-06-04 11:25:11 | auto push

---
+++ other/go_over/2024/6月/2024年6月4日/1.js
@@ -0,0 +1,7 @@
+let a = {
+  b:1,
+  c:2
+}
+let x = 2;
+module.exports.a = a;
+exports.x = x;
---
+++ other/go_over/2024/6月/2024年6月4日/main.js
@@ -0,0 +1,6 @@
+let a = require('./1')
+let aa = require('./1')
+a.a.b=333
+a.x = 343;
+console.log(a)
+console.log(aa)
