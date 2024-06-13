9beac3da73b1b16945af03c47035c8c862c9cbc3
zhaohs <console.log@163.com>
2024-06-13 16:28:37 +0800

    2024-06-13 16:28:32 | auto push

--- other/go_over/2024/6月/2024年6月13日/main.js
+++ other/go_over/2024/6月/2024年6月13日/main.js
@@ -1,18 +1,21 @@
 import puppeteer from "puppeteer";
 
-let browser =  await puppeteer.launch({
+let browser = await puppeteer.launch({
     headless: false,
     defaultViewport: {
-        width: 0,
+        width: 3000,
         height: 0,
     },
 });
 let page = await browser.newPage();
-await page.goto("https://www.baidu.com/s?wd=classin&rsv_spt=1&rsv_iqid=0xd3f05a1e0010296e&issp=1&f=3&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=&tn=68018901_16_pg&ch=&rsv_enter=1&rsv_btype=i&prefixsug=&rsp=0&rsv_dl=ih_0");
-
-await page.waitForSelector('.c-container')
-let a = await page.$eval('.c-container  h3', (el) =>{
-  console.log(el)
-  return el
+await page.goto("https://areschang.top/web/html/");
+await page.waitForSelector("group a");
+await page.evaluate(()=>{
+    let listItems = document.querySelectorAll('group a')
+    for(let a of listItems){
+        if(a.textContent.includes('语义化')){
+          a.click()
+          break;
+        }
+    }
 })
-console.log(a)
