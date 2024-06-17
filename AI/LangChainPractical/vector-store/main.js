04ccd9f67f3f7e30f00c59bf724c0a3fcad07caf
zhaohs <console.log@163.com>
2024-06-17 18:00:19 +0800

    2024-06-17 18:00:14 | auto push

--- AI/LangChainPractical/vector-store/main.js
+++ AI/LangChainPractical/vector-store/main.js
@@ -1,21 +1,25 @@
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as openai from "langchain/openai";
-import * as faiss from "faiss-node";
-let { FaissStore } = faiss;
-let { OpenAIEmbeddings } = openai;
-const run = async () => {
-    const loader = new TextLoader("../Embedding/data/kong.txt");
-    const docs = await loader.load();
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 100,
-        chunkOverlap: 20,
-    });
-    const splitDocs = await splitter.splitDocuments(docs);
+import "dotenv/config";
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
-    const embeddings = new OpenAIEmbeddings();
-    const vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
-    const directory = "../db/kongyiji";
-    await vectorStore.save(directory)
+const run = async () => {
+  const loader = new TextLoader("../data/kong.txt");
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 20,
+  });
+
+  const splitDocs = await splitter.splitDocuments(docs);
+
+  const embeddings = new OpenAIEmbeddings();
+  const vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
+
+  const directory = "../db/kongyiji";
+  await vectorStore.save(directory);
 };
+
 run();
