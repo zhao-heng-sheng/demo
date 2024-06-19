66cd91907739ef11c912a636889f033a4f2d2ed2
zhaohs <console.log@163.com>
2024-06-19 17:19:17 +0800

    2024-06-19 17:19:12 | auto push

--- other/go_over/2024/6月/2024年6月18日/promise.js
+++ other/go_over/2024/6月/2024年6月18日/promise.js
@@ -3,6 +3,11 @@
 2. 当Promise状态为fulfilled执行成功回调，为rejected执行失败回调；
 3. 如resolve或reject在定时器里，则定时器结束后再执行then；
 4. then支持链式调用，下一次then执行受上一次then返回值的影响；
+
+1. then方法本身会返回一个新的Promise对象；
+2. 如果返回值是promise对象，返回值为成功，新promise就是成功；
+3. 如果返回值是promise对象，返回值为失败，新promise就是失败；
+4. 如果返回值非promise对象，新promise对象就是成功，值为此返回值；
 */
 class MyPromise {
     constructor(executor) {
@@ -31,21 +36,56 @@ class MyPromise {
         this.onRejectedCallbacks.forEach((cb) => cb(this.result));
     }
     then(onFulfilled, onRejected) {
-        if (this.state === "fulfilled") {
-            onFulfilled(this.result);
-        }
-        if (this.state === "rejected") {
-            onRejected(this.result);
-        }
-        if(this.state === "padding") {
-            this.onFulfilledCallbacks.push(onFulfilled.bind(this));
-            this.onRejectedCallbacks.push(onRejected.bind(this));
-        }
-        return 1;
+        let thenPromise = new MyPromise((resolve, reject) => {
+            const resolvePromise = (cb) => {
+                try {
+                    const x = cb(this.result);
+                    if (x instanceof MyPromise) {
+                        x.then(resolve, reject);
+                    }
+                    resolve(x);
+                } catch (error) {
+                    reject(err);
+                    throw new Error(err);
+                }
+            };
+            if (this.state === "fulfilled") {
+                resolvePromise(onFulfilled);
+            }
+            if (this.state === "rejected") {
+                resolvePromise(onRejected);
+                // onRejected(this.result);
+            }
+            if (this.state === "padding") {
+                this.onFulfilledCallbacks.push(resolvePromise.bind(this, onFulfilled));
+                this.onRejectedCallbacks.push(resolvePromise.bind(this, onRejected));
+            }
+        });
+
+        return thenPromise;
     }
 }
-const test2 = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('success') // 1秒后输出 success
-    }, 1000)
-}).then(res => console.log(res), err => console.log(err))
+
+const test3 = new MyPromise((resolve, reject) => {
+    resolve(100); // 输出 状态：success 值： 200
+})
+    .then(
+        (res) => 2 * res,
+        (err) => 3 * err
+    )
+    .then(
+        (res) => console.log("success", res),
+        (err) => console.log("fail", err)
+    );
+
+const test4 = new MyPromise((resolve, reject) => {
+    resolve(100); // 输出 状态：fail 值：200
+})
+    .then(
+        (res) => new MyPromise((resolve, reject) => reject(2 * res)),
+        (err) => new Promise((resolve, reject) => resolve(3 * err))
+    )
+    .then(
+        (res) => console.log("success", res),
+        (err) => console.log("fail", err)
+    );
