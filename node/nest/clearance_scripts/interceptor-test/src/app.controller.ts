7cb7d90abf5a891e397e74b056938b87748cd88e
zhaohs <console.log@163.com>
2024-07-02 16:18:33 +0800

    2024-07-02 16:18:12 | auto push

--- node/nest/clearance_scripts/interceptor-test/src/app.controller.ts
+++ node/nest/clearance_scripts/interceptor-test/src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaInterceptor } from './aaa.interceptor';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseInterceptors(AaaInterceptor)
   getHello(): string {
     return this.appService.getHello();
   }
--- node/nest/clearance_scripts/interceptor-test/src/main.ts
+++ node/nest/clearance_scripts/interceptor-test/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AaaInterceptor } from './aaa.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new AaaInterceptor());
   await app.listen(3000);
 }
 bootstrap();
