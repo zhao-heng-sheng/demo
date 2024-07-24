1008927d23aaea37377aeab5bde939058d30d7db
zhaohs <console.log@163.com>
2024-07-24 13:18:07 +0800

    2024-07-24 13:18:02 | auto push

--- node/git-auto-push/config.js
+++ node/git-auto-push/config.js
@@ -5,6 +5,10 @@ export default {
   },
   // 需要执行的文件夹
   folders: [
-    `C:/project/demo`
+    `C:/project/demo`,
+    `C:/project/zhaohs`,
+    `C:/project/zhaohsAdmin`,
+    `C:/project/win-tasker`,
+    `E:/project/notes`,
   ],
 };
