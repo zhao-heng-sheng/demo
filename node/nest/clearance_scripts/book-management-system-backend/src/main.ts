b7683ba4608b6463660f846c0ad3ba6ec89b8008
zhaohs <console.log@163.com>
2024-07-26 15:19:25 +0800

    2024-07-26 15:19:19 | auto push

--- node/nest/clearance_scripts/book-management-system-backend/src/main.ts
+++ node/nest/clearance_scripts/book-management-system-backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import {join} from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
   await app.listen(3000);
 }
 bootstrap();
