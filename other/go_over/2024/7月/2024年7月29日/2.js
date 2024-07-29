8c8c39c75c2693fc8566a206c0d530a99a05f332
zhaohs <console.log@163.com>
2024-07-29 17:57:52 +0800

    2024-07-29 17:57:48 | auto push

---
+++ other/go_over/2024/7月/2024年7月29日/2.js
@@ -0,0 +1,8 @@
+import unoconv from 'unoconv-promise'
+// let filePath = await unoconv.run({
+//   file:'./1.docx',
+// })
+// console.log(filePath)
+
+// let format = await unoconv.formats()
+console.log(123)
